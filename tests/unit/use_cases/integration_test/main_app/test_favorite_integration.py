35cff52a0be104063bf9fb1ce4b9c35434b7cfc8
Rybakov Artem Oleekseevich <vp41919@gmail.com>
2024-10-10 13:25:17 +0300

    fix: unit integration-test favorite.

--- tests/unit/use_cases/integration_test/main_app/test_favorite_integration.py
+++ tests/unit/use_cases/integration_test/main_app/test_favorite_integration.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from core.apps.main.models.products import Products
+from core.apps.main.models.favorites import Favorites
 from core.api.v1.main.dto.responses import DTOResponseFavoriteAPI
 from core.apps.main.services.favorites.favorites import (
     ORMFavoriteProductsIdsFilterService,
 )
+from core.apps.main.models.products import CategoriesProduct
 from core.apps.main.services.universal import (
     ORMAllProductsService,
     ORMFavoriteProductsIdsService,
@@ -14,8 +17,23 @@ from core.apps.main.use_cases.favorite import (
 )
 
 
+User = get_user_model()
+
+
 class TestFavoritePageCommandHandlerIntegration(TestCase):
     def setUp(self):
+        self.user = User.objects.create(username="test_user")
+        self.categories = CategoriesProduct.objects.create(category="all")
+
+        self.product_1 = Products.objects.create(
+            name="Product 1",
+            category_id=self.categories.id,
+        )
+        self.product_2 = Products.objects.create(
+            name="Product 2",
+            category_id=self.categories.id,
+        )
+
         self.favorite_products_service_ids = ORMFavoriteProductsIdsService()
         self.get_all_products_service = ORMAllProductsService()
         self.products_service = ORMFavoriteProductsIdsFilterService()
@@ -27,8 +45,6 @@ class TestFavoritePageCommandHandlerIntegration(TestCase):
         )
 
     def test_handle_unauthenticated_user(self):
-        """Проверяем, что неавторизованный пользователь получает пустой список
-        продуктов."""
         command = FavoritePageCommand(is_authenticated=False)
 
         response = self.command_handler.handle(command)
@@ -38,13 +54,7 @@ class TestFavoritePageCommandHandlerIntegration(TestCase):
         self.assertEqual(response.products, [])
 
     def test_handle_user_with_no_favorites(self):
-        """Проверяем, что авторизованный пользователь без избранного получает
-        пустой список продуктов."""
         command = FavoritePageCommand(is_authenticated=True, username="test_user")
-
-        self.favorite_products_service_ids.get_ids_products_in_favorite = (
-            lambda username: []
-        )
 
         response = self.command_handler.handle(command)
 
@@ -53,26 +63,24 @@ class TestFavoritePageCommandHandlerIntegration(TestCase):
         self.assertEqual(response.products, [])
 
     def test_handle_user_with_favorites(self):
-        """Проверяем, что авторизованный пользователь с избранными продуктами
-        получает отфильтрованный список."""
         command = FavoritePageCommand(is_authenticated=True, username="test_user")
 
-        favorite_products_ids = [1, 2, 3]
-        self.favorite_products_service_ids.get_ids_products_in_favorite = (
-            lambda username: favorite_products_ids
-        )
+        Favorites.objects.create(user=self.user, product_id=self.product_1.id)
+        Favorites.objects.create(user=self.user, product_id=self.product_2.id)
 
         all_products = [
-            {"id": 1, "title": "Product 1"},
-            {"id": 2, "title": "Product 2"},
+            {"id": self.product_1.id, "title": "Product 1"},
+            {"id": self.product_2.id, "title": "Product 2"},
             {"id": 4, "title": "Product 4"},
         ]
+
         self.get_all_products_service.get_all_products = lambda: all_products
 
         filtered_products = [
-            {"id": 1, "title": "Product 1"},
-            {"id": 2, "title": "Product 2"},
+            {"id": self.product_1.id, "title": "Product 1"},
+            {"id": self.product_2.id, "title": "Product 2"},
         ]
+
         self.products_service.get_filtered_products_by_favorite_ids = (
             lambda products, ids_products_in_favorite: filtered_products
         )
